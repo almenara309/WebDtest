659239f8b4af56e6e1f5fedbb5784d1639acfeb0
Carlos C <carlosclaret@outlook.es>
2017-02-03 03:26:05 +0100

    Empezando Web

--- start.js
+++ start.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 
 var app = express();
-
+app.set("view engine","jade");
 app.use(express.static("public"));
 app.listen(8080);
+
+app.get("/",function(solicitud,respuesta){
+	respuesta.render("index");
+});
 
 console.log("Servidor web iniciado");
